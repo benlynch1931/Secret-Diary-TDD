ec1e7b2529e18fea593c76f312fceb39e6c08bfa
Ben <ben.lynch1931@gmail.com>
2020-09-30 20:33:08 +0100

    GREEN Test: Status is true when .lock is run
    Finished in 0.00153 seconds (files took 0.2352 seconds to load)
    1 example, 0 failures

--- lib/secret_diary.rb
+++ lib/secret_diary.rb
@@ -1,11 +1,15 @@
 class Diary
   attr_reader :locked_status
 
-  def initialize
-    @locked_status = true
-  end
+  # def initialize
+  #   @locked_status = true
+  # end
 
   def unlock
     @locked_status = false
+  end
+
+  def lock
+    @locked_status = true
   end
 end
