ed3a268bc23cbf97946f4ff251cc125b4d70c488
Ben <ben.lynch1931@gmail.com>
2020-10-01 09:02:09 +0100

    FORMAT: Nested describe statements for each area of Diary.rb

--- spec/secret_diary_spec.rb
+++ spec/secret_diary_spec.rb
@@ -2,50 +2,59 @@ require 'secret_diary.rb'
 
 describe Diary do
 
-  it "returns locked status of diary" do
-    subject = Diary.new
-    expect(subject.locked_status).to eq true
+  describe "#locked_status" do
+
+    it "returns locked status of diary" do
+      subject = Diary.new
+      expect(subject.locked_status).to eq true
+    end
+
+    it "responds to an .unlock method" do
+      expect(subject).to respond_to(:unlock)
+    end
+
+    it "sets locked status to false when run .unlock" do
+      subject = Diary.new
+      subject.unlock
+      expect(subject.locked_status).to eq false
+    end
+
+  # For this, I had removed the init method to ensure lock method works
+    it "sets locked status to true when run .unlock" do
+      subject = Diary.new
+      subject.lock
+      expect(subject.locked_status).to eq true
+    end
+
+    it "sets locked status to true when run .unlock then .lock" do
+      subject = Diary.new
+      subject.unlock
+      subject.lock
+      expect(subject.locked_status).to eq true
+    end
   end
 
-  it "responds to an .unlock method" do
-    expect(subject).to respond_to(:unlock)
+  describe "#add_entry" do
+
+    it "responds to an .add_entry method" do
+      expect(subject).to respond_to(:add_entry)
+    end
+
+    it "returns false to .add_entry as locked_status is true" do
+      expect(subject.add_entry).to eq false
+    end
+
+    it "returns true to .add_entry when .unlock is run first" do
+      subject = Diary.new
+      subject.unlock
+      expect(subject.add_entry).to eq true
+    end
   end
 
-  it "sets locked status to false when run .unlock" do
-    subject = Diary.new
-    subject.unlock
-    expect(subject.locked_status).to eq false
-  end
+  describe "#get_entries" do
 
-# For this, I had removed the init method to ensure lock method works
-  it "sets locked status to true when run .unlock" do
-    subject = Diary.new
-    subject.lock
-    expect(subject.locked_status).to eq true
-  end
-
-  it "sets locked status to true when run .unlock then .lock" do
-    subject = Diary.new
-    subject.unlock
-    subject.lock
-    expect(subject.locked_status).to eq true
-  end
-
-  it "responds to an .add_entry method" do
-    expect(subject).to respond_to(:add_entry)
-  end
-
-  it "returns false to .add_entry as locked_status is true" do
-    expect(subject.add_entry).to eq false
-  end
-
-  it "returns true to .add_entry when .unlock is run first" do
-    subject = Diary.new
-    subject.unlock
-    expect(subject.add_entry).to eq true
-  end
-
-  it "responds to an .get_entries method" do
-    expect(subject).to respond_to(:get_entries)
+    it "responds to an .get_entries method" do
+      expect(subject).to respond_to(:get_entries)
+    end
   end
 end
