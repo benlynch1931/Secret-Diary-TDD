0a11502280b12fe15e17a5045ca07ef7490f5d51
Ben <ben.lynch1931@gmail.com>
2020-10-01 11:54:43 +0100

    Refactored tests using DRY principle and upd recent test so it works

--- spec/secret_diary_spec.rb
+++ spec/secret_diary_spec.rb
@@ -9,9 +9,6 @@ describe Diary do
       expect(subject.locked_status).to eq true
     end
 
-    it "responds to an .unlock method" do
-      expect(subject).to respond_to(:unlock)
-    end
 
     it "sets locked status to false when run .unlock" do
       subject = Diary.new
@@ -25,30 +22,21 @@ describe Diary do
       subject.lock
       expect(subject.locked_status).to eq true
     end
-
-    it "sets locked status to true when run .unlock then .lock" do
-      subject = Diary.new
-      subject.unlock
-      subject.lock
-      expect(subject.locked_status).to eq true
-    end
   end
 
   describe "#add_entry" do
 
-    it "responds to an .add_entry method" do
-      expect(subject).to respond_to(:add_entry)
-    end
+
 
     it "returns false to .add_entry as locked_status is true" do
       expect(subject.add_entry).to eq false
     end
 
-    # it "returns true to .add_entry when .unlock is run first" do
-    #   subject = Diary.new
-    #   subject.unlock
-    #   expect(subject.add_entry).to eq true
-    # end
+    it "returns nil array to .add_entry when .unlock is run first" do
+      subject = Diary.new
+      subject.unlock
+      expect(subject.add_entry).to eq [nil]
+    end
 
     it "adds an entry to the diary" do
       subject = Diary.new
@@ -66,18 +54,15 @@ describe Diary do
 
   describe "#get_entries" do
 
-    it "responds to an .get_entries method" do
-      expect(subject).to respond_to(:get_entries)
-    end
 
     it "returns false to .get_entries as locked_status is true" do
       expect(subject.get_entries).to eq false
     end
 
-    it "returns true to .get_entries when .unlock is run first" do
+    it "returns empty array to .get_entries when .unlock is run first" do
       subject = Diary.new
       subject.unlock
-      expect(subject.get_entries).to eq true
+      expect(subject.get_entries).to eq []
     end
 
     it "`put`es entries added when .get_entry called" do
@@ -85,7 +70,7 @@ describe Diary do
       subject.unlock
       subject.add_entry("Entry 1")
       subject.add_entry("Entry 2")
-      expect(subject.get_entries).to eq "Entries:\n \n Entry 1\n ----\n Entry 2\n ----"
+      expect {subject.get_entries}.to output("Entries: \n \nEntry 1\n----\nEntry 2\n----\n").to_stdout
     end
   end
 end
